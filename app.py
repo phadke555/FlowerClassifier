886a7e2c2d48e3cd80ca250dae15a222610a4b81
Rohan Phadke <rohanphadke@users.noreply.huggingface.co>
2023-05-09 20:55:05 +0000

    Upload app.py

--- app.py
+++ app.py
@@ -1,18 +1,55 @@
+# from fastai.vision.all import *
+# import gradio as gr
+
+# learn = load_learner('model.pkl')
+
+# categories = 'Sunflower', 'Orchid', 'Rose'
+
+# def classify_image(img):
+#     pred, idx, probs = learn.predict(img)
+#     return(dict(zip(categories, map(float, probs))))
+
+# image = gr.inputs.Image(shape=(192,192))
+# label = gr.outputs.label()
+# examples = ['sunflower.jpeg', 'orchid.jpeg', 'rose.jpeg']
+
+# intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+# intf.launch(inline=False, share=True)
+
 from fastai.vision.all import *
 import gradio as gr
 
+# Load the pre-trained model
 learn = load_learner('model.pkl')
 
-categories = 'Sunflower', 'Orchid', 'Rose'
+# Define the categories that the model can classify
+categories = ['Sunflower', 'Orchid', 'Rose']
 
+# Define the function to classify an image and return the predicted category label
 def classify_image(img):
     pred, idx, probs = learn.predict(img)
-    return(dict(zip(categories, map(float, probs))))
+    return categories[idx]
 
-image = gr.inputs.Image(shape=(192,192))
-label = gr.outputs.label()
-examples = ['sunflower.jpeg', 'orchid.jpeg', 'rose.jpeg']
+# Define the input and output types for the Gradio interface
+image_input = gr.inputs.Image(shape=(224, 224))
+label_output = gr.outputs.Label(num_top_classes=len(categories))
 
-intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
-intf.launch(inline=False, share=True)
+# Define example images for the interface
+examples = [
+    ['sunflower.jpeg'],
+    ['orchid.jpeg'],
+    ['rose.jpeg']
+]
 
+# Create the Gradio interface
+interface = gr.Interface(
+    fn=classify_image, 
+    inputs=image_input, 
+    outputs=label_output,
+    examples=examples,
+    title="Image Classifier",
+    description="This app classifies images into three categories: Sunflower, Orchid, and Rose."
+)
+
+# Launch the interface
+interface.launch()
