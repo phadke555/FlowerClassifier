5986c74cc56af93e78dd64314b6bd272124d4b3b
Rohan Phadke <rohanphadke@users.noreply.huggingface.co>
2023-05-09 19:06:59 +0000

    Upload app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def predict(img):
     return {labels[i]: float(probs[i]) for i in range(len(labels))}
 
 import gradio as gr
-gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(512, 512)), outputs=gr.outputs.Label(num_top_classes=3)).launch(share=True)
+gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(512, 512)), outputs=gr.outputs.Label(num_top_classes=3)).launch()
