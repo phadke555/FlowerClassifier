d93e85b857d723af280f09b20375f3470185dc35
Rohan Phadke <rohanphadke@users.noreply.huggingface.co>
2023-05-09 22:03:38 +0000

    Upload 2 files

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from fastai.vision.all import *
 import gradio as gr
 
-learn = load_learner('model.pkl')
+learn = load_learner('model_extended.pkl')
 
-categories = 'Sunflower', 'Orchid', 'Rose'
+# categories = 'Sunflower', 'Orchid', 'Rose'
 
 def classify_image(img):
     pred, idx, probs = learn.predict(img)
-    return(dict(zip(categories, map(float, probs))))
+    return pred
 
 image = gr.inputs.Image(shape=(192,192))
 label = gr.outputs.Label()
